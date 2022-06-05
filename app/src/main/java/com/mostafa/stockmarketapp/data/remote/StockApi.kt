f7ff32b6833f807532e39c9bd795c207348d9a90
mostafaa95 <mostafa95.gamal@gmail.com>
2022-06-05 21:21:44 +0200

    added two functions to remote stockApi to get intradayinfo and companyinfo

--- app/src/main/java/com/mostafa/stockmarketapp/data/remote/StockApi.kt
+++ app/src/main/java/com/mostafa/stockmarketapp/data/remote/StockApi.kt
@@ -10,6 +10,20 @@ interface StockApi {
     suspend fun getListings(
         @Query("apikey") apiKey: String = API_KEY
     ): ResponseBody
+
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntradayInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    ): ResponseBody
+
+    @GET("query?function=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    )
+
+
     companion object {
         const val API_KEY = "Q4LMOCLVZTTZTVNT"
         const val BASE_URL = "https://alphavantage.co"
