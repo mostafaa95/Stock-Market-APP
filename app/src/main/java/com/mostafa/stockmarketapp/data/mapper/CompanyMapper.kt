9dd56d66d336df31a2f57efca6e6fe6b3a59c9b4
mostafaa95 <mostafa95.gamal@gmail.com>
2022-06-05 21:07:56 +0200

    added a mapper to companyinfoDto to a domain level companyinfo object

--- app/src/main/java/com/mostafa/stockmarketapp/data/mapper/CompanyMapper.kt
+++ app/src/main/java/com/mostafa/stockmarketapp/data/mapper/CompanyMapper.kt
@@ -1,6 +1,8 @@
 package com.mostafa.stockmarketapp.data.mapper
 
 import com.mostafa.stockmarketapp.data.local.CompanyListingEntity
+import com.mostafa.stockmarketapp.data.remote.dto.CompanyInfoDto
+import com.mostafa.stockmarketapp.domain.model.CompanyInfo
 import com.mostafa.stockmarketapp.domain.model.CompanyListing
 
 fun CompanyListingEntity.toCompanyListing(): CompanyListing {
@@ -16,5 +18,15 @@ fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
         name = name,
         symbol = symbol,
         exchange = exchange
+    )
+}
+
+fun CompanyInfoDto.toComapnyIndo(): CompanyInfo {
+    return CompanyInfo(
+        symbol = symbol ?: "",
+        description = description ?: "",
+        name = name ?: "",
+        country = country ?: "",
+        industry = industry ?: ""
     )
 }
